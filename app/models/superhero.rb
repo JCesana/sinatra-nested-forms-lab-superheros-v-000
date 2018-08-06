00c645dbd8f03bd2ade4a4626431cffb92f41bc3
Josh Cesana <joshuacesana@gmail.com>
2018-08-06 02:25:22 +0000

    automatically backed up by learn

---
+++ app/models/superhero.rb
@@ -0,0 +1,14 @@
+class Superhero
+  attr_accessor :name, :power, :bio 
+  @@all = [] 
+  
+  def initialize(name, power, bio)
+    @name = name 
+    @power = power 
+    @bio = bio 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+end 
