53e0d81ad31548509b4498cdb7760149e77e3538
Josh Cesana <joshuacesana@gmail.com>
2018-08-06 02:24:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+    get '/' do 
+      erb :index 
+    end 
+    
+    get '/team/new' do 
+      erb :new 
+    end 
+    
+    post '/teams' do 
+      @team = params
+      
+      erb :show
+    end 
+    
 end
