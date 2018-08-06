5b41dda5993b11c8abe1a52dec96df61b6cfe19f
Josh Cesana <joshuacesana@gmail.com>
2018-08-06 02:33:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,9 @@ class App < Sinatra::Base
       @team = Team.new(params[:team][:name], params[:team][:motto])
       binding.pry 
       params[:team][:members].each do |details|
+        binding.pry 
         Superhero.new(details)
+        binding.pry 
       end 
       binding.pry 
       @heros = Superhero.all 
